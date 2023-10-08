4181315fe782b7c1f5faf37879d7a952db2b4386
Vkhrabrov <perepel@gmail.com>
2023-10-08 21:06:15 +0200

    the basic Lexer is functional

--- minishell.c
+++ minishell.c
@@ -3,31 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkhrabro <vkhrabro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vadimhrabrov <vadimhrabrov@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 18:55:56 by vkhrabro          #+#    #+#             */
-/*   Updated: 2023/10/07 02:02:48 by vkhrabro         ###   ########.fr       */
+/*   Updated: 2023/10/08 21:00:55 by vadimhrabro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void create_empty_list(void)
+// void create_empty_list(void)
+// {
+//     token.value = NULL;
+//     token.type = NULL;
+// }
+
+const char* token_type_to_string(tokentype type) 
 {
-    token.value = NULL;
-    token.type = NULL;
+    switch (type) 
+    {
+        case TOKEN_COMMAND: return "COMMAND";
+        case TOKEN_ARGUMENT: return "ARGUMENT";
+        case TOKEN_PIPE: return "PIPE";
+        case TOKEN_REDIRECT_IN: return "REDIRECT_IN";
+        case TOKEN_REDIRECT_OUT: return "REDIRECT_OUT";
+        // ... add other cases as needed
+        default: return "UNKNOWN";
+    }
 }
 
-Token* create_token(const char* content, TokenType type) 
+void print_token(token *t) 
+{
+    printf("Token content: '%s', Token type: %s\n", t->content, token_type_to_string(t->type));
+}
+
+token   *create_token(const char* content, tokentype type) 
 {
     // Allocate memory for the Token structure
-    Token* new_token = (Token*)malloc(sizeof(Token));
+    token   *new_token = (token*)malloc(sizeof(token));
     if (!new_token) 
     {
         perror("Failed to allocate memory for token");
         exit(EXIT_FAILURE);
     }
-
     // Allocate memory for the content of the token and copy the given content into it
     new_token->content = ft_strdup(content);
     if (!new_token->content) {
@@ -37,19 +55,33 @@ Token* create_token(const char* content, TokenType type)
     }
     // Set the type of the token
     new_token->type = type;
+    new_token->next = NULL;
     return (new_token);
 }
 
+void add_to_list(token **head, token *new_token) 
+{
+    if (!*head) 
+    {
+        *head = new_token;
+        return;
+    }
+    token* temp = *head;
+    while (temp->next)
+        temp = temp->next;
+    temp->next = new_token;
+    print_token(new_token);
+}
 
-char *substring(input_string, start, i)
+char *substring(char *input_string, int start, int end)
 {
     char *substring;
     int k;
     int t;
 
-    k = i - start + 1;
+    k = end - start + 1;
     t = 0;
-    substring = malloc((sizeof)(char) * k + 1);
+    substring = malloc(sizeof(char) * k + 1);
     while (t < k)
         substring[t++] = input_string[start++];
     substring[t] = '\0';
@@ -58,22 +90,27 @@ char *substring(input_string, start, i)
 
 void tokenization(char *input)
 {
-    char* input_string;
-    int i;
-    List* tokens;
+    char    *input_string;
+    int     i;
+    token   *tokens;
+    char    *quoted_string;
+    char    *command_or_arg;
 
     input_string = input;
-    token = create_empty_list();
+    tokens = NULL;
 
     // Now, let's iterate over the input string, character by character
     i = 0;
-    while (i < strlen(input_string)) 
+    while (i < (int)ft_strlen(input_string)) 
     {
         char c = input_string[i];
 
         // If the character is a space, we simply skip to the next character.
         if (c == ' ')
+        {
+            i++;
             continue;
+        }
 
         // If the character is a quote, we should keep reading characters 
         // until we find the closing quote.
@@ -81,16 +118,16 @@ void tokenization(char *input)
         {
             int start = i + 1; // Start just after the quote
             i++; // Move to next character    
-            while (input_string[i] != '"' && i < strlen(input_string)) 
+            while (input_string[i] != '"' && i < (int)ft_strlen(input_string)) 
                 i++;
             // Now extract the substring between start and i
-            char* quoted_string = substring(input_string, start, i);
-            add_to_list(tokens, create_token(quoted_string, TOKEN_ARGUMENT));
+            quoted_string = substring(input_string, start, i);
+            add_to_list(&tokens, create_token(quoted_string, TOKEN_ARGUMENT));
         }
         // Handling other token types (like pipes or redirects) can be done in a similar manner.
         // For example, if we encounter a '|', we can add a TOKEN_PIPE token to our list.
         else if (c == '|')
-            add_to_list(tokens, create_token("|", TOKEN_PIPE));
+            add_to_list(&tokens, create_token("|", TOKEN_PIPE));
         // Here, you would handle other special characters similarly...
 
         // ...
@@ -103,11 +140,11 @@ void tokenization(char *input)
             
             // Read until we encounter a space or another special character.
             // This is where you'd extend checks for other special characters too.
-            while (input_string[i] != ' ' && input_string[i] != '|' && i < strlen(input_string))
+            while (input_string[i] != ' ' && input_string[i] != '|' && i < (int)ft_strlen(input_string))
                 i++;
 
-            char* command_or_arg = substring(input_string, start, i - 1);
-            add_to_list(tokens, create_token(command_or_arg, /* decide if it's a command or argument */));
+            command_or_arg = substring(input_string, start, i - 1);
+            add_to_list(&tokens, create_token(command_or_arg, TOKEN_COMMAND));
             i--; // Because the outer for-loop will increment i again
         }
         i++;
@@ -117,13 +154,10 @@ void tokenization(char *input)
 
 int main(int argc, char **argv, char **envp)
 {
-    int i;
-
-    i = 0;
     (void)argc;
     (void)argv;
     (void)envp;
-    init_signals();
+    // init_signals();
     while (1) 
     {
         char *input = readline("minishell> ");
--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkhrabro <vkhrabro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vadimhrabrov <vadimhrabrov@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 22:58:44 by vkhrabro          #+#    #+#             */
-/*   Updated: 2023/10/07 02:03:33 by vkhrabro         ###   ########.fr       */
+/*   Updated: 2023/10/08 20:51:37 by vadimhrabro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <signal.h>
+#include "libft/libft.h"
 
 typedef enum 
 {
@@ -27,15 +28,18 @@ typedef enum
     TOKEN_REDIRECT_IN,
     TOKEN_REDIRECT_OUT,
     // ... add any other token types you need
-}             TokenType;
+}             tokentype;
 
-typedef struct 
+typedef struct token token;  // Forward declaration
+
+struct token
 {
-    char        *content;
-    TokenType   type;
-}                 Token;
+    char       *content;
+    tokentype   type;
+    token      *next;
+};
 
 void        init_signals(void);
 void        rl_replace_line (const char *text, int clear_undo);
 
-#endef 
+#endif
