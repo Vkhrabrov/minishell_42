7d74c51f2174295e1d10d64769a1ab1be70bd5ae
Carlos Carracedo garcia-villalba <ccg-v@hotmail.com>
2023-12-01 01:22:39 +0100

    C. EXPORT: breaks when splitting into smaller functions (does not print/add new vars)

--- builtins/export_builtin_BROKEN.c
+++ builtins/export_builtin_BROKEN.c
@@ -1,21 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   export_builtin.c                                   :+:      :+:    :+:   */
+/*   export_builtin_BROKEN.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 21:00:11 by ccarrace          #+#    #+#             */
-/*   Updated: 2023/12/01 01:15:10 by ccarrace         ###   ########.fr       */
+/*   Updated: 2023/12/01 00:03:33 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-//ESTA VERSION AUN NO SEPARA FUNCIONES, PERO APARENTEMENTE EL PROBLEMA DE IMPRESION
-// DE VARIABLES SIN VALOR SE HA SOLUCIONADO
-// export A1 ya guarda la variable y la muestra en la lista
-// (el problema estaba en set_new_var, rama crear nueva variable, la igualdad
-// (equal_sign_position == arg_len) no se cumple con variables sin valor porque
-// equal_sign_position es 0)
 
 #include "../minishell.h"
 
@@ -27,9 +20,6 @@ void	print_export_list(t_env_lst **env_lst)
     while (current != NULL)
     {
         printf("declare -x %s", current->var_name);
-		// if (current->var_value != NULL)
-        // 	printf("=\"%s", current->var_value);
-        // printf("\"\n");
 		if (current->var_value != NULL)
         	printf("=\"%s\"\n", current->var_value);
 		else
@@ -38,6 +28,13 @@ void	print_export_list(t_env_lst **env_lst)
     }
 }
 
+/* is_var_name_valid()
+ * 
+ *	- Only alphabetical characters or underscore allowed 
+ *		at the beginning of the name
+ *	- Only alphanumerical characters or underscore allowed
+ *		in the rest of the name
+ */
 static bool	is_var_name_valid(char *arg, int equal_sign_position)
 {
 	int	i;
@@ -55,24 +52,24 @@ static bool	is_var_name_valid(char *arg, int equal_sign_position)
 	return (true);
 }
 
-
-
-
-static void	update_existing_node(t_env_lst **head, char *arg, size_t equal_sign_position, size_t arg_len)
+/* update_existing_node()
+ *
+ *	- Searches for an existing node with the same name as provided in arg
+ *	- If a node with the same name exists, updates its value
+ *		-- If no value is provided in arg, sets it to NULL
+ *	- If the node is already updated, exits
+ */
+static int update_existing_node(t_env_lst **head, char *arg, size_t equal_sign_position, size_t arg_len)
 {
-    while (*head != NULL)
+    while (*head != NULL) 
 	{
-        if (ft_strncmp((*head)->var_name, arg, equal_sign_position) == 0 &&
-            (*head)->var_name[equal_sign_position] == '\0')
-			{
-            // Node with the same name exists, update its value
+        if (ft_strncmp((*head)->var_name, arg, equal_sign_position) == 0
+            && (*head)->var_name[equal_sign_position] == '\0')
+		{
             if (equal_sign_position == arg_len)
 			{
-printf("Found coincidence: %s | %s\n", (*head)->var_name, arg);
-                // If no value provided in arg, set it to NULL
                 if ((*head)->var_value != NULL)
 				{
-printf("arg has no value, %s will be set to NULL\n", arg);
                     free((*head)->var_value);
                     (*head)->var_value = NULL;
                 }
@@ -82,86 +79,58 @@ printf("arg has no value, %s will be set to NULL\n", arg);
                 free((*head)->var_value);
                 (*head)->var_value = ft_strdup(arg + equal_sign_position + 1);
             }
-            // return (0); // Node already updated, no need to create a new one
+            return (EXIT_SUCCESS);
         }
         head = &(*head)->next;
     }
-	// return (1);	
+    return (EXIT_FAILURE);
 }
 
+/* create_new_node()
+ *	(is set_new_var part 1/3)
+ *	- If no node exists with the same name as provided in arg, creates it
+ */
+static int create_new_node(t_env_lst **head, char *arg, size_t equal_sign_position)
+{
+    t_env_lst *new_node = malloc(sizeof(t_env_lst));
+    if (new_node == NULL)
+        return (EXIT_FAILURE);
 
-
-
+    if (equal_sign_position == 0)
+	{
+        new_node->var_name = ft_strdup(arg);
+        new_node->var_value = NULL;
+    }
+	else
+	{
+        new_node->var_name = ft_substr(arg, 0, equal_sign_position);
+        new_node->var_value = ft_strdup(arg + equal_sign_position + 1);
+    }
+    new_node->next = *head;
+    *head = new_node;
+    return (EXIT_SUCCESS);
+}
 
 static int set_new_var(t_env_lst **head, char *arg)
 {
-    size_t 	equal_sign_position = find_char_index(arg, '=');
-    size_t 	arg_len = ft_strlen(arg);
-	int		exit_status;
+    size_t equal_sign_position = find_char_index(arg, '=');
+    size_t arg_len = ft_strlen(arg);
 
-	exit_status = 0;
-	if (is_var_name_valid(arg, equal_sign_position) == false)
-		return (build_error_msg("export: ", arg, ": not a valid identifier", true));
-    // Search for an existing node with the same name
-    // while (*head != NULL)
-	// {
-    //     if (ft_strncmp((*head)->var_name, arg, equal_sign_position) == 0 &&
-    //         (*head)->var_name[equal_sign_position] == '\0')
-	// 		{
-    //         // Node with the same name exists, update its value
-    //         if (equal_sign_position == arg_len)
-	// 		{
-    //             // If no value provided in arg, set it to NULL
-    //             if ((*head)->var_value != NULL)
-	// 			{
-    //                 free((*head)->var_value);
-    //                 (*head)->var_value = NULL;
-    //             }
-    //         }
-	// 		else
-	// 		{
-    //             free((*head)->var_value);
-    //             (*head)->var_value = ft_strdup(arg + equal_sign_position + 1);
-    //         }
-    //         return (0); // Node already updated, no need to create a new one
-    //     }
-    //     head = &(*head)->next;
-    // }
-    // If no existing node with the same name, create a new node
-	if (equal_sign_position != 0)
-	{
-		update_existing_node(head, arg, equal_sign_position, arg_len);
-printf("equal_sign_position = %zu -> %s: node already exists\n", equal_sign_position, arg);
-    } 
-	else
-	{
-printf("equal_sign_position = %zu -> %s: node does not exist\n", equal_sign_position, arg);		
-		t_env_lst *new_node = malloc(sizeof(t_env_lst));
-		if (new_node == NULL)
-			return (1);
-		if (equal_sign_position == 0)
-		{
-printf("%s: arg comes with no value, it will be set to NULL\n", arg);
-			new_node->var_name = ft_strdup(arg);
-			new_node->var_value = NULL;
-		}
-		else
-		{
-printf("%s: arg has a value, new var with value should be created\n", arg);
-			new_node->var_name = ft_substr(arg, 0, equal_sign_position);
-			new_node->var_value = ft_strdup(arg + equal_sign_position + 1);
-		}
-		new_node->next = *head;
-		*head = new_node;
-	}
-	return (0);
+    if (is_var_name_valid(arg, equal_sign_position) == false)
+        return (build_error_msg("export: ", arg, MS_INVALIDID, true));
+    if (update_existing_node(head, arg, equal_sign_position, arg_len) == 0)
+        return (0);
+    return (create_new_node(head, arg, equal_sign_position));
 }
+
 
 t_env_lst *insert_sorted(t_env_lst **head, t_env_lst *new_node)
 {
     t_env_lst *current = *head;
     t_env_lst *prev = NULL;
 
+    // it doesn't matter whether the length we pass to ft_strncomp is 
+	// 'strlen(current->var_name)' or 'strlen(new_node->var_name)'
 	while (current != NULL && ft_strncmp(new_node->var_name, current->var_name, strlen(current->var_name)) > 0)
 	{
         prev = current;
@@ -169,11 +138,13 @@ t_env_lst *insert_sorted(t_env_lst **head, t_env_lst *new_node)
     }
     if (prev == NULL)
 	{
+        // Insert at the beginning
         new_node->next = *head;
         *head = new_node;
     }
 	else
 	{
+        // Insert in the middle or at the end
         prev->next = new_node;
         new_node->next = current;
     }
@@ -214,7 +185,7 @@ int	create_sorted_env_list(t_env_lst *env_lst)
  */
 int export_builtin(t_env_lst *env_lst, token *args_lst)
 {
-    if (args_lst && ft_list_size(args_lst) > 0)
+     if (args_lst && ft_list_size(args_lst) > 0)
 	{
         while (args_lst != NULL)
 		{
@@ -225,8 +196,26 @@ int export_builtin(t_env_lst *env_lst, token *args_lst)
     }
 	else
 	{
+        // // Create a new linked list to store the sorted environment variables
+        // sorted_list = NULL;
+        // // Copy all of the environment variables to the new linked list
+        // while (env_lst != NULL) {
+        //     new_node = malloc(sizeof(t_env_lst));
+        //     if (new_node == NULL)
+        //         return (EXIT_FAILURE);
+        //     new_node->var_name = ft_strdup(env_lst->var_name);
+		// 	if (env_lst->var_value == NULL)
+		// 		new_node->var_value = NULL;
+		// 	else
+        //     	new_node->var_value = ft_strdup(env_lst->var_value);
+			
+        //     new_node->next = NULL;
+        //     sorted_list = insert_sorted(&sorted_list, new_node);
+        //     env_lst = env_lst->next;
+        // }
 		if (create_sorted_env_list(env_lst) != 0)
 			return (EXIT_FAILURE);
     }
     return (EXIT_SUCCESS);
 }
+
---
+++ builtins/export_builtin_COPY.c
@@ -0,0 +1,200 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export_builtin.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 21:00:11 by ccarrace          #+#    #+#             */
+/*   Updated: 2023/11/29 00:34:09 by ccarrace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	print_export_list(t_env_lst **env_lst)
+{
+	t_env_lst *current;
+	
+	current = *env_lst;
+    while (current != NULL)
+    {
+        printf("declare -x %s", current->var_name);
+		// if (current->var_value != NULL)
+        // 	printf("=\"%s", current->var_value);
+        // printf("\"\n");
+		if (current->var_value != NULL)
+        	printf("=\"%s\"\n", current->var_value);
+		else
+        	printf("\n");		
+        current = current->next;       
+    }
+}
+
+/* is_var_name_valid()
+ * 
+ *	- Only alphabetical characters or underscore allowed 
+ *		at the beginning of the name
+ *	- Only alphanumerical characters or underscore allowed
+ *		in the rest of the name
+ */
+static bool	is_var_name_valid(char *arg, int equal_sign_position)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(arg[i]) && arg[i] != '_')
+		return (false);
+	i++;
+	while (i < equal_sign_position)
+	{
+		if (!ft_isalnum(arg[i]) && arg[i] != '_')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+static int set_new_var(t_env_lst **head, char *arg)
+{
+    size_t 	equal_sign_position = find_char_index(arg, '=');
+    size_t 	arg_len = ft_strlen(arg);
+	int		exit_status;
+
+	exit_status = 0;
+	if (is_var_name_valid(arg, equal_sign_position) == false)
+	{
+		exit_status = build_error_msg("export: ", arg, ": not a valid identifier", true);
+		// printf ("exit_status = %d\n", exit_status);
+		if (exit_status == 1)
+			exit(1);
+		// printf("exit_status sent from 'set_new_var' = %d\n", exit_status);
+		// return(exit_status);
+	}
+    // Search for an existing node with the same name
+    while (*head != NULL)
+	{
+        if (ft_strncmp((*head)->var_name, arg, equal_sign_position) == 0 &&
+            (*head)->var_name[equal_sign_position] == '\0')
+			{
+            // Node with the same name exists, update its value
+            if (equal_sign_position == arg_len)
+			{
+                // If no value provided in arg, set it to NULL
+                if ((*head)->var_value != NULL)
+				{
+                    free((*head)->var_value);
+                    (*head)->var_value = NULL;
+                }
+            }
+			else
+			{
+                free((*head)->var_value);
+                (*head)->var_value = ft_strdup(arg + equal_sign_position + 1);
+            }
+            return (0); // Node already updated, no need to create a new one
+        }
+        head = &(*head)->next;
+    }
+    // If no existing node with the same name, create a new node
+    t_env_lst *new_node = malloc(sizeof(t_env_lst));
+    if (new_node == NULL)
+        return (1);
+    if (equal_sign_position == arg_len)
+	{
+        new_node->var_name = ft_strdup(arg);
+        new_node->var_value = NULL;
+    }
+	else
+	{
+        new_node->var_name = ft_substr(arg, 0, equal_sign_position);
+        new_node->var_value = ft_strdup(arg + equal_sign_position + 1);
+    }
+    // new_node->next = NULL;
+	new_node->next = *head;
+    *head = new_node;
+	return (0);
+}
+
+t_env_lst *insert_sorted(t_env_lst **head, t_env_lst *new_node)
+{
+    t_env_lst *current = *head;
+    t_env_lst *prev = NULL;
+
+    // it doesn't matter whether the length we pass to ft_strncomp is 
+	// 'strlen(current->var_name)' or 'strlen(new_node->var_name)'
+	while (current != NULL && ft_strncmp(new_node->var_name, current->var_name, strlen(current->var_name)) > 0)
+	{
+        prev = current;
+        current = current->next;
+    }
+    if (prev == NULL)
+	{
+        // Insert at the beginning
+        new_node->next = *head;
+        *head = new_node;
+    }
+	else
+	{
+        // Insert in the middle or at the end
+        prev->next = new_node;
+        new_node->next = current;
+    }
+	return (*head);
+}
+
+int export_builtin(t_env_lst *env_lst, token *args_lst)
+{
+    t_env_lst	*sorted_list;
+	t_env_lst *new_node;
+
+    if (args_lst && ft_list_size(args_lst) > 0)
+	{
+        while (args_lst != NULL)
+		{
+            if (set_new_var(&env_lst, args_lst->content) == 1)
+			{
+				// printf("set_new_var() returned 1\n");
+				return (1);
+			}
+			// printf("set_new_var() returned 0\n");
+            args_lst = args_lst->next;
+        }
+        // print_export_list(&env_lst);
+    }
+	else
+	{
+        // Create a new linked list to store the sorted environment variables
+        sorted_list = NULL;
+        // Copy all of the environment variables to the new linked list
+        while (env_lst != NULL) {
+            new_node = malloc(sizeof(t_env_lst));
+            if (new_node == NULL)
+                return (1);
+            new_node->var_name = ft_strdup(env_lst->var_name);
+
+			if (env_lst->var_value == NULL)
+				new_node->var_value = NULL;
+			else
+            	new_node->var_value = ft_strdup(env_lst->var_value);
+			
+            new_node->next = NULL;
+            sorted_list = insert_sorted(&sorted_list, new_node);
+            env_lst = env_lst->next;
+        }
+        // Print the sorted list of environment variables
+        print_export_list(&sorted_list);
+        // Free the sorted list of environment variables
+        // while (sorted_list != NULL)
+		// {
+        //     t_env_lst *next_node = sorted_list->next;
+        //     free(sorted_list->var_name);
+		// 	if (sorted_list->var_value != NULL)
+        //     	free(sorted_list->var_value);
+        //     free(sorted_list);
+        //     sorted_list = next_node;
+        // }
+    }
+    return (0);
+}
+
