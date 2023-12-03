276cad1fc3b9c5cc96cad3a9edb38b721169ff17
Carlos Carracedo garcia-villalba <ccg-v@hotmail.com>
2023-12-04 00:52:02 +0100

    C. EXIT: chasing leaks

--- builtins/exit_builtin_COPY.c
+++ builtins/exit_builtin_COPY.c
@@ -6,31 +6,27 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 13:40:46 by ccarrace          #+#    #+#             */
-/*   Updated: 2023/12/04 00:41:50 by ccarrace         ###   ########.fr       */
+/*   Updated: 2023/12/03 20:49:30 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 // #include <ctype.h>
 
-int	calculate_exit_status(long num)
+void	calculate_exit_status(long num)
 {
-	int	exit_status;
-
 	if (num > 0)
-		exit_status = num % 256;
+		exit(num % 256);
 	else if (num < 0)
-		exit_status = 256 - ((num * -1) % 256);
+		exit(256 - ((num * -1) % 256));
 	else
-		exit_status = 0;
-	return (exit_status);
+		exit(EXIT_SUCCESS);
 }
 
-int	handle_single_argument(token *args_lst)
+void	handle_single_argument(token *args_lst)
 {
 	long	num;
 	char	*str;
-	int		exit_status;
 
 	remove_leading_zeros(args_lst->content);
 	remove_trailing_spaces(args_lst->content);
@@ -45,45 +41,48 @@ int	handle_single_argument(token *args_lst)
 	{
 		ft_putstr_fd("exit\n", 2);
 		build_error_msg("exit: ", args_lst->content, MS_NOTNUMARG, false);
-		exit_status = 255;
+		exit(255);
 	}
 	else
-		exit_status = calculate_exit_status(num);
-	return (exit_status);
+		calculate_exit_status(num);
 }
 
-int	handle_multiple_arguments(token *args_lst)
+void	handle_multiple_arguments(token *args_lst)
 {
+	pid_t	pid;
 	size_t	i;
-	int		exit_status;
+	int		status;
 
-	i = 1;
-	exit_status = 1;
-	while (i < ft_strlen(args_lst->content))
+	i = -1;
+	while (++i < ft_strlen(args_lst->content))
 	{
 		if (!ft_isdigit(args_lst->content[i]))
 		{
 			ft_putstr_fd("exit\n", 2);
 			build_error_msg("exit: ", args_lst->content, MS_NOTNUMARG, false);
-			exit_status = 255;
-			break;
+			exit(255);
 		}
-		i++;
 	}
-	if (exit_status != 255)
-		exit_status = build_error_msg("exit: ", NULL, MS_TOOMANYARG, false);
-	return (exit_status);
+	pid = fork();
+	if (pid == 0)
+	{
+		build_error_msg("exit: ", NULL, MS_TOOMANYARG, false);
+		exit(1);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		g_exitstatus = WEXITSTATUS(status);
+	}
 }
 
 int	exit_builtin(token *args_lst)
 {
-	int	exit_status;
 	if (args_lst == NULL)
 		exit(EXIT_SUCCESS);
 	if (ft_list_size(args_lst) == 1)
-		exit_status = handle_single_argument(args_lst);
+		handle_single_argument(args_lst);
 	else
-		exit_status = handle_multiple_arguments(args_lst);
-	// free_args_list(args_lst);
-	return (exit_status);
+		handle_multiple_arguments(args_lst);
+	return (EXIT_FAILURE);
 }
