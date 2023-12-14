f779931a5b7c07afbe0a3930e90aacb4732ac3a2
Carlos Carracedo garcia-villalba <ccg-v@hotmail.com>
2023-12-15 00:58:46 +0100

    C. norminette OK

--- env_list_creation.c
+++ env_list_creation.c
@@ -6,91 +6,89 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 20:53:39 by vkhrabro          #+#    #+#             */
-/*   Updated: 2023/12/06 23:58:50 by ccarrace         ###   ########.fr       */
+/*   Updated: 2023/12/15 00:51:20 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void t_env_init( t_env_lst  *env_lst)
-// {
-//     env_lst->var_name = NULL;
-//     env_lst->var_value = NULL;
-// }
-
-void add_env_var_to_list(t_env_lst **head, const char *envp_line)
+void	add_var_with_value(const char *envp_line, t_env_lst *new_node, \
+		size_t start_pos, size_t end_pos)
 {
-    t_env_lst	*new_node;
-    size_t		equal_sign_position;
-    size_t		envp_line_len;
-
-    new_node = malloc(sizeof(t_env_lst));
-    if (new_node == NULL)
-		return ;
-    equal_sign_position = find_char_index(envp_line, '=');
-    envp_line_len = ft_strlen(envp_line);
-    if (equal_sign_position == envp_line_len)
-	{
-        new_node->var_name = ft_strdup(envp_line);
-        new_node->var_value = NULL;
-    }
+	new_node->var_name = ft_substr(envp_line, 0, start_pos - 1);
+	if (ft_strncmp(new_node->var_name, "OLDPWD", \
+			find_max_len(new_node->var_name, "OLDPWD")) == 0)
+		new_node->var_value = NULL;
 	else
-	{
-        new_node->var_name = ft_substr(envp_line, 0, equal_sign_position);
-		if (ft_strncmp(new_node->var_name, "OLDPWD", find_max_len(new_node->var_name, "OLDPWD")) == 0)
-        	new_node->var_value = NULL;
-		else
-        	new_node->var_value = ft_substr(envp_line, equal_sign_position + 1, envp_line_len - equal_sign_position);
-		// if (ft_strncmp(new_node->var_name, "SHLVL", find_max_len(new_node->var_name, "SHLVL")) == 0)
-		// 	new_node->var_value = "0";
+		new_node->var_value = ft_substr(envp_line, start_pos, end_pos);
+}
 
-    }
+/*	create_env_node()
+ *
+ *	for each line in the array of environment variables (envp)
+ *	- it creates a node 
+ *	- finds the position of the equal character (=)
+ *		-- if there is no equal, stores the name of the variable
+ *			in the node, and sets NULL as its value
+ *		-- if there is an equal, stores name and value in the
+ *			node (see 'add_var_with_value()' function above)
+ */
+t_env_lst	*create_env_node(const char *envp_line)
+{
+	t_env_lst	*new_node;
+	size_t		equal_sign_position;
+	size_t		envp_line_len;
+
+	new_node = malloc(sizeof(t_env_lst));
+	if (new_node == NULL)
+		return (NULL);
+	equal_sign_position = find_char_index(envp_line, '=');
+	envp_line_len = ft_strlen(envp_line);
+	if (equal_sign_position == envp_line_len)
+	{
+		new_node->var_name = ft_strdup(envp_line);
+		new_node->var_value = NULL;
+	}
+	else
+		add_var_with_value(envp_line, new_node, equal_sign_position + 1, \
+			envp_line_len - equal_sign_position);
 	new_node->next = NULL;
-  	if (*head == NULL)
-        *head = new_node;
+	return (new_node);
+}
+
+void	append_to_list(t_env_lst **head, t_env_lst *new_node)
+{
+	t_env_lst	*current;
+
+	if (*head == NULL)
+		*head = new_node;
 	else
 	{
-		t_env_lst	*current = *head;
+		current = *head;
 		while (current->next != NULL)
 			current = current->next;
 		current->next = new_node;
 	}
 }
 
+void	add_env_var_to_list(t_env_lst **head, const char *envp_line)
+{
+	t_env_lst	*new_node;
+
+	new_node = create_env_node(envp_line);
+	if (new_node == NULL)
+		return ;
+	append_to_list(head, new_node);
+}
+
 void	save_env_list(t_env_lst **env_lst, char **envp)
 {
 	int	i;
+
 	i = 0;
-    while (envp[i] != NULL)
-    {
-        add_env_var_to_list(env_lst, envp[i]);
-        i++;       
-    }
-}
-
-void	print_env_list(t_env_lst **env_lst)
-{
-	t_env_lst *current;
-	
-	current = *env_lst;
-    while (current != NULL)
-    {
-        printf("%s=%s\n", current->var_name, current->var_value);
-        current = current->next;       
-    }
-}
-
-void	free_env_list(t_env_lst *env_lst)
-{
-    t_env_lst *temp;
-
-    while (env_lst != NULL)
+	while (envp[i] != NULL)
 	{
-        temp = env_lst;
-        env_lst = env_lst->next;
-        free(temp->var_name);
-		if (temp->var_value != NULL)
-			free(temp->var_value);
-        free(temp);
-    }
+		add_env_var_to_list(env_lst, envp[i]);
+		i++;
+	}
 }
--- lexer.c
+++ lexer.c
@@ -3,48 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkhrabro <vkhrabro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 21:51:03 by vkhrabro          #+#    #+#             */
-/*   Updated: 2023/12/13 23:20:32 by vkhrabro         ###   ########.fr       */
+/*   Updated: 2023/12/14 21:16:20 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	handle_quoted_string(char c, char *input, tokenizer_state *state)
+void	handle_single_redirection(char *redir, char c, tokenizer_state *state)
 {
-	char		end_char;
-	char		*arg;
-	tokentype	current_type;
-
-	end_char = c;
-	arg = lex_quoted_string(input, &(state->i), end_char);
-	current_type = T_ARG;
-	if (ft_strlen(arg) == 0 && state->prev_type == T_CMD)
-	{
-		current_type = T_ARG;
-	}
-	else if (ft_strlen(arg) == 0 && ft_strlen(input) == 2)
-	{
-		current_type = T_CMD;
-	}
-	else if (ft_strlen(arg) == 0 && ft_strlen(input) > 2)
-		return ;
-	
-	if ((state->prev_type == T_PIPE || state->prev_type == T_NONE) && ft_strlen(arg) != 0)
-	{
-		current_type = T_CMD;
-		state->expect_command = 0;
-	}
-	else if ((state->prev_type == T_REDIR_IN || state->prev_type == T_REDIR_OUT) && ft_strlen(arg) != 0)
-
-		current_type = T_ARG;
-	else if (state->prev_type == T_VAR_EXP && ft_strlen(arg) != 0)
-		current_type = T_ENV_VAR;
-	add_to_list(&(state->tokens), creat_token(arg, current_type));
-	free(arg);
-	state->prev_type = current_type;
+	ft_strlcpy(redir, (char []){c, '\0', '\0'}, sizeof(redir));
+	if (c == '>')
+		add_to_list(&(state->tokens), creat_token(redir, T_REDIR_OUT));
+	else
+		add_to_list(&(state->tokens), creat_token(redir, T_REDIR_IN));
 }
 
 void	handle_redirection_end(char c, tokenizer_state *state)
@@ -56,39 +30,32 @@ void	handle_redirection_end(char c, tokenizer_state *state)
 	state->expect_filename_after_redir = 1;
 }
 
-void handle_redirection(char c, char *input, tokenizer_state *state)
+void	handle_redirection(char c, char *input, tokenizer_state *state)
 {
-    char next_char;
-    char redir[3];
+	char	next_char;
+	char	redir[3];
 
-    next_char = input[state->i + 1];
-    if ((c == '>' && next_char == '>') || (c == '<' && next_char == '<'))
-    {
-        redir[0] = c;
-        redir[1] = next_char;
-        redir[2] = '\0';
-        if (c == '>')
-            add_to_list(&(state->tokens), creat_token(redir, T_APP_REDIR));
-        else
-        {
-            add_to_list(&(state->tokens), creat_token(redir, T_HEREDOC));
-            state->prev_type = T_HEREDOC;  // Set the state here for HEREDOC
-            state->i++;  // Increment the index
-            return;  // Exit the function, so we don't reset the state in handle_redirection_end
-        }
-        state->i++;
-    }
-    else
-    {
-        ft_strlcpy(redir, (char []){c, '\0', '\0'}, sizeof(redir));
-        if (c == '>')
-            add_to_list(&(state->tokens), creat_token(redir, T_REDIR_OUT));
-        else
-            add_to_list(&(state->tokens), creat_token(redir, T_REDIR_IN));
-    }
-    handle_redirection_end(c, state);
+	next_char = input[state->i + 1];
+	if ((c == '>' && next_char == '>') || (c == '<' && next_char == '<'))
+	{
+		redir[0] = c;
+		redir[1] = next_char;
+		redir[2] = '\0';
+		if (c == '>')
+			add_to_list(&(state->tokens), creat_token(redir, T_APP_REDIR));
+		else
+		{
+			add_to_list(&(state->tokens), creat_token(redir, T_HEREDOC));
+			state->prev_type = T_HEREDOC;
+			state->i++;
+			return ;
+		}
+		state->i++;
+	}
+	else
+		handle_single_redirection(redir, c, state);
+	handle_redirection_end(c, state);
 }
-
 
 void	handle_special_tokens(char c, char *input, tokenizer_state *state)
 {
@@ -105,27 +72,26 @@ void	handle_special_tokens(char c, char *input, tokenizer_state *state)
 	else if (c == '$' && next_char == '?')
 	{
 		redir[0] = c;
-		redir[1] = next_char;    
+		redir[1] = next_char;
 		redir[2] = '\0';
 		add_to_list(&(state->tokens), creat_token(redir, T_EXIT_STATUS));
 		state->i++;
 	}
 	else if (c == '$')
 	{
-		add_to_list(&(state->tokens),
-			creat_token("$", T_VAR_EXP));
+		add_to_list(&(state->tokens), creat_token("$", T_VAR_EXP));
 		state->prev_type = T_VAR_EXP;
 	}
 	else if (if_redir(c))
 		handle_redirection(c, input, state);
 }
-void print_tokens(const token *tokens) {
-    while (tokens != NULL) {
-        printf("Token: '%s', Type: %s\n", tokens->content, token_type_to_string(tokens->type));
-        tokens = tokens->next;
-    }
-}
 
+/*	tokenization()
+ *
+ *	to print tokens for debugging, add
+ *		print_tokens(state.tokens)
+ *	at the end of the function
+*/
 token	*tokenization(char *input)
 {
 	tokenizer_state	state;
@@ -148,6 +114,5 @@ token	*tokenization(char *input)
 			handle_commands_and_args(input, &state);
 		state.i++;
 	}
-	// print_tokens(state.tokens);
 	return (state.tokens);
 }
